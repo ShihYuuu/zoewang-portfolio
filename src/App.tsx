89eb027bcef1aaf637eea730b7f2d0d2efaf5acf
Zoe Wang <67854884+ShihYuuu@users.noreply.github.com>
2025-05-25 16:00:57 +0000

    add basename in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/zoewang-portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/article/:id" element={<Article />} />
